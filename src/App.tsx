4200f7e9e90f9a184d47dd75502a681584fae428
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-09-07 15:03:07 +0500

    add new codes for start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,7 @@
-import React, { useRef, useState } from "react";
-import "./App.scss";
+import React from 'react'
 
-// import RandomColor from "./hex-btn";
-
-interface AppProps {}
-
-export interface Mode {
-  counter: number;
-}
-
-const App: React.FC<AppProps> = () => {
-  // const { color, generateColor } = RandomColor();
-  const [state, setState] = useState({
-    counter: 0,
-    step: false,
-  });
-
-  const inputDelay = useRef<HTMLInputElement>(null);
-  const inputStep = useRef<HTMLInputElement>(null);
-
-  const counter = (option: number) => {
-    setState({ ...state, step: true });
-    setTimeout(() => {
-      setState({
-        ...state,
-        counter: state.counter + +inputStep.current?.value! * option,
-        step: false,
-      });
-    }, +inputDelay.current?.value! * 1000);
-  };
-
+export default function App() {
   return (
-    // <div
-    //   className="App"
-    //   style={{
-    //     backgroundColor: "#" + color,
-    //   }}
-    // >
-    //   <button className="btn" onClick={generateColor}>
-    //     RandomColor
-    //   </button>
-    // </div>
-
-    <div></div>
-  );
-};
-
-export default App;
+    <div>App</div>
+  )
+}
