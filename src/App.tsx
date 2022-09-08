4e17fd8a47561f8f5d7fdccc8b27a1d11f3d1fce
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-09-08 20:43:28 +0500

    add new codes for advanced tic-tac-toe folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import Game from "./pages/Game";
-import Start from "./pages/Start";
-import Finished from "./pages/Finished";
-import useTickTackToe from "./hooks/useTicTacToe";
+import React, { useState, ReactElement } from "react";
+import "./App.scss";
+
 const App = () => {
-  const game = useTickTackToe();
-  return (
-    <div className="App">
-      {game.status === "created" && <Start handleStart={game.handleStart} />}
-      {game.status === "finished" && (
-        <Finished name={game.winner} restart={game.handleRestart} />
-      )}
-      {game.status === "started" && (
-        <Game board={game.board} handleClick={game.handleClick} />
-      )}
-    </div>
-  );
+  return <div className="App"></div>;
 };
 export default App;
